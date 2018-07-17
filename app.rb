c6e4458efa76ee805672880e35d19d2a7fbbcba2
oishidass <oishidass1234@gmail.com>
2018-07-17 19:43:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  gets '/' 
+  gets '/reversename/:name' 
+  @name.params[:names]
+  @name.reverse
 
 end
+
+gets '/squaare/:number'
+@param[:num1].to_i
